ad0205e9c0e193b4f789888fde0d4ed3bb7b42dd
Aveek Saha <aveek.s98@gmail.com>
2021-05-19 23:06:46 +0530

    Create basic bot

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.DISCORD_BOT_SECRET;
+
+client.on('ready', () => {
+  console.log("I'm in");
+  console.log(client.user.username);
+});
+
+client.on('message', msg => {
+    if (msg.author.id != client.user.id) {
+        msg.channel.send(msg.content.split('').reverse().join(''));
+    }
+});
+
+client.login(token);
