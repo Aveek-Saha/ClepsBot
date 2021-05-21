a991bcb22d12672e7a65c9f28dc4c99cd6a6482e
Aveek Saha <aveek.s98@gmail.com>
2021-05-21 10:30:42 +0530

    List all members in all VCs

--- index.js
+++ index.js
@@ -7,20 +7,28 @@ const token = process.env.DISCORD_BOT_SECRET;
 const prefix = '!'
 
 client.on('ready', () => {
-  console.log("I'm in");
-  console.log(client.user.username);
+  console.log(client.user.username, "is running");
 });
 
 client.on('message', message => {
     if (message.content.startsWith(prefix) && (message.author.id != client.user.id)) {
-        const args = message.content.slice(prefix.length).trim().split(' ');
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
 	    const command = args.shift().toLowerCase();
 
         if (command === 'teams') {
             if (!args.length) {
                 return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+            } else if (!parseInt(args[0])) {
+                return message.channel.send(`Please provide a number, ${message.author}!`);
             }
-    
+            const channels = message.guild.channels.cache.filter(c => c.type === 'voice');
+            for (const [channelID, channel] of channels) {
+                for (const [memberID, member] of channel.members) {
+                    console.log(member.user.username);
+                }
+              }
+            // console.log(channels);
+            // console.log(client.channels.cache.array());
             message.channel.send(`First argument: ${args[0]}`);
         }
     }
